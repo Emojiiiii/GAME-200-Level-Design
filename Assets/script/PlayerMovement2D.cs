d92ba48ca7d6e937ea97613095fcbdc9f503a5bc
Emojiiiii <114673122+Emojiiiii@users.noreply.github.com>
2026-02-10 17:17:08 -0500

    movement and camera following script

---
+++ Assets/script/PlayerMovement2D.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+}
